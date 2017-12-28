a5f33ebe7cd5654730a5785331a2ff1326a52383
tikTokTik <mithoonk@flock.com>
2017-12-28 13:03:11 +0530

    Add master project

---
+++ master-project/master-project/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<list>
+#include<set>
+#include<unordered_map>
+#include<sstream>
+#include<limits.h>
+#include<stdio.h>
+using namespace std;
+
+
+int main(){
+    freopen("/Users/mithoonkumar/Documents/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
+    
+    
+    
+    return 0;
+}
+
