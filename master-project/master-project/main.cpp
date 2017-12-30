5c2a5209a92a4984fbac67b3f7bbbc9e2af989b1
tikTokTik <mithoonk@flock.com>
2017-12-30 20:04:43 +0530

    Complete code with half test case pass

--- master-project/master-project/main.cpp
+++ master-project/master-project/main.cpp
@@ -17,8 +17,37 @@ using namespace std;
 
 int main(){
     freopen("/Users/mithoonkumar/Documents/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
+    map<string,int> dayHash;
+     dayHash["mon"]=0;
+     dayHash["tues"]=1;
+     dayHash["wed"]=2;
+     dayHash["thurs"]=3;
+     dayHash["fri"]=4;
+     dayHash["sat"]=5;
+     dayHash["sun"]=6;
     
-    
+    int t;
+    cin>>t;
+    for(int tc= 0 ;tc<t; tc++){
+        int n;
+        string str;
+        cin>>n>>str;
+        int ans[7];
+        for(int i=0;i<7;i++)
+        {
+            ans[i]=4;
+        }
+        for(int i=dayHash[str];i<(n%28)+dayHash[str];i++)
+        {
+            ans[i]++;
+        }
+        for(int i=0;i<7;i++)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+
     
     return 0;
 }
