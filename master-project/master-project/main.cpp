d2eaacd479161c62f23a1c860ee247c8823ed4fc
tikTokTik <mithoonk@flock.com>
2017-12-31 13:04:15 +0530

    Complete allPerm code

--- master-project/master-project/main.cpp
+++ master-project/master-project/main.cpp
@@ -14,12 +14,42 @@
 #include<stdio.h>
 using namespace std;
 
+void findAllPerm(string str,vector<string>&allPerm,int pos,int len,string tempPerm,bool *visited)
+{
+    if(pos == len)
+    {
+        allPerm.push_back(tempPerm);
+        return;
+    }
+    
+    for(int i=0;i<str.length();i++)
+    {
+      if(!visited[i])
+      {
+          tempPerm[pos] = str[i];
+          visited[i] = true;
+          findAllPerm(str, allPerm, pos+1, len, tempPerm,visited);
+          visited[i] = false;
+      }
+    }
+    
+}
 
 int main(){
     freopen("/Users/mithoonkumar/Documents/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
-    
-    
-    
+    string str;
+    cin>>str;
+    vector<string>allPerm;
+    bool visited[str.length()];
+    for(int i=0;i<str.length();i++)
+    {
+        visited[i] = false;
+    }
+    findAllPerm(str,allPerm,0,str.length(),str,visited);
+    for(int i=0;i<allPerm.size();i++)
+    {
+        cout<<allPerm[i]<<endl;
+    }
     return 0;
 }
 
