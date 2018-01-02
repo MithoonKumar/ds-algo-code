160cc744a02ef86284d63418fd898befba1242e5
tikTokTik <mithoonk@flock.com>
2018-01-02 18:59:30 +0530

    Add code to improve input and output time

--- master-project/master-project/main.cpp
+++ master-project/master-project/main.cpp
@@ -13,11 +13,12 @@
 #include<limits.h>
 #include<stdio.h>
 using namespace std;
+#define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
 
 
 int main(){
     freopen("/Users/mithoonkumar/Documents/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
-    
+    faster;
     
     
     return 0;
