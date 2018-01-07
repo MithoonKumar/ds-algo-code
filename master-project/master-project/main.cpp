da36d46d1359d2db819c70b7ceb47a1b05aa289c
tikTokTik <mithoonk@flock.com>
2018-01-07 16:41:08 +0530

    Solve

--- master-project/master-project/main.cpp
+++ master-project/master-project/main.cpp
@@ -19,6 +19,31 @@ using namespace std;
 int main(){
     freopen("/Users/mithoonkumar/Documents/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
     faster;
+    int t;
+    cin>>t;
+    for(int tc=0;tc<t;tc++)
+    {
+        int arr[4];
+        map<int,int>hashMap;
+        cin>>arr[0]>>arr[1]>>arr[2]>>arr[3];
+        for(int i=0;i<4;i++)
+        {
+            hashMap[arr[i]]=0;
+        }
+        for(int i=0;i<4;i++)
+        {
+            hashMap[arr[i]]++;
+        }
+        int count =0;
+        for(int i=0;i<4;i++)
+        {
+            count +=hashMap[arr[i]];
+        }
+        if(count ==8 || count ==16)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
     
     
     return 0;
