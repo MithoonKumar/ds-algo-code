9f7b5ab0693f5f3f6f294dbadf1d2e66a92331d1
tikTokTik <mithoonk@flock.com>
2018-08-22 15:35:26 +0530

    Pass all cases

--- master-project/master-project/main.cpp
+++ master-project/master-project/main.cpp
@@ -17,10 +17,32 @@ using namespace std;
 
 
 int main(){
-    freopen("/Users/mithoonkumar/Documents/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
+    freopen("/Users/mithoonkumar/Documents/personal-github-repo/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
     faster;
-    
-    
+    string str;
+    cin>>str;
+    int n = (int) str.length();
+    int maxValue = 1, counter = 0, count = 0;
+    int tempLength = 1;
+    while(1) {
+        count++;
+        if (str[counter] != str[(counter+1)%n]) {
+            tempLength++;
+        } else {
+            maxValue = tempLength > maxValue ? tempLength: maxValue;
+            tempLength = 1;
+        }
+        counter++;
+        counter = counter%n;
+        if (tempLength == n) {
+            maxValue = n;
+            break;
+        }
+        if (count == 2*n-1) {
+            break;
+        }
+    }
+    cout<<maxValue<<endl;
     return 0;
 }
 
