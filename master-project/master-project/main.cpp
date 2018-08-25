0182a7353248483122294283f38e1d42fb34002f
tikTokTik <mithoonk@flock.com>
2018-08-25 11:22:58 +0530

    Pass all cases

--- master-project/master-project/main.cpp
+++ master-project/master-project/main.cpp
@@ -17,10 +17,40 @@ using namespace std;
 
 
 int main(){
-    freopen("/Users/mithoonkumar/Documents/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
+    //freopen("/Users/mithoonkumar/Documents/personal-github-repo/ds-algo-code/ds-algo-code/master-project/master-project/input.txt","r",stdin);
     faster;
-    
+    long long n, q;
+    cin>>n>>q;
+    long long midValue, squaredValue;
+    squaredValue= n*n;
+    if ((squaredValue%2) == 1) {
+        midValue = squaredValue / 2 +1;
+    } else {
+        midValue = squaredValue / 2;
+    }
+    while (q--) {
+        long long x, y;
+        cin>>x>>y;
+        long long prevRect = (x-1) * n;
+        long long count1;
+        if (((x+y)%2) == 0) {
+            if ((prevRect%2) == 1) {
+                count1 = prevRect / 2 +1;
+            } else {
+                count1 = prevRect / 2;
+            }
+        } else {
+            count1 = midValue;
+            count1 += prevRect/2;
+        }
+        long long count2;
+        if ((y%2) == 1 ) {
+            count2 = y / 2 +1;
+        } else {
+            count2 = y / 2;
+        }
+        cout<<count1 + count2<<endl;
+    }
     
     return 0;
 }
-
